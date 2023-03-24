5ddda3b81239513a2600b9182a341fc8b535c7bd
Lucas Freire <lucfre7@gmail.com>
2023-03-24 14:57:36 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('#anotar')
+const lista = document.querySelector('#lista');
+const boxTarefa = document.querySelector('#tarefa')
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+tarefas.forEach( (tarefa) => {
+
+    createCheck(tarefa)
+    
+});
+
+form.addEventListener('submit', (evento) =>{
+
+    evento.preventDefault();
+
+    const tarefa = evento.target.elements['tarefa'];
+
+    tarefas.push(tarefa.value)
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    if (tarefa.value == ''){
+
+        boxTarefa.style.borderColor = 'red'
+
+    } else{
+
+        boxTarefa.style.borderColor = '#271e1d'
+        createCheck(tarefa.value)
+    }
+
+    tarefa.value = ''
+
+})
+
+function createCheck(tarefa){
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('typing-animation')
+    novoItem.innerHTML += tarefa;
+    novoItem.appendChild(btnCheck())
+    lista.appendChild(novoItem)
+
+}
+
+function btnCheck(){
+
+    const elementoBtn = document.createElement('button')
+    elementoBtn.classList.add('uil-check')
+
+    elementoBtn.addEventListener("click", function(){
+        checkElement(this.parentNode, this.parentNode.firstChild.data)
+    })
+
+    return elementoBtn
+
+}
+
+function checkElement(tag, elemento){
+
+    tag.remove()
+    let position = tarefas.indexOf(elemento)
+    tarefas.splice(position, 1)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+}
